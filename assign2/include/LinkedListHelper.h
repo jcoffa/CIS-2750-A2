d28aa7271900d4594a35f6734deeecf5ef90601c
Joseph Coffa <josephco440@gmail.com>
2019-02-11 23:06:16 -0500

    Linked List helper functions, so far to iterate through a List backwards

---
+++ assign2/include/LinkedListHelper.h
@@ -0,0 +1,14 @@
+/************************************
+ *  Name: Joseph Coffa              *
+ *  Student #: 1007320              *
+ *  Due Date: February 27, 2019     *
+ *                                  *
+ *  Assignment 2, CIS*2750          *
+ *  LinkedListHelper.h              *
+ ************************************/
+
+#include "LinkedListAPI.h"
+
+ListIterator createReverseIterator(List *list);
+
+void *previousElement(ListIterator *iter);
