bc4c1e3deddec397e37a8718a0dedd5c2ef0939f
Joseph Coffa <josephco440@gmail.com>
2019-02-26 17:53:03 -0500

    Files used for the development of this assignment have been moved to a new directory, assign2Production, and the former assign2 directory is now used for submission

---
+++ assign2Production/include/LinkedListHelper.h
@@ -0,0 +1,19 @@
+/************************************
+ *  Name: Joseph Coffa              *
+ *  Student #: 1007320              *
+ *  Due Date: February 27, 2019     *
+ *                                  *
+ *  Assignment 2, CIS*2750          *
+ *  LinkedListHelper.h              *
+ ************************************/
+
+#ifndef LINKEDLISTHELPER_H
+#define LINKEDLISTHELPER_H
+
+#include "LinkedListAPI.h"
+
+ListIterator createReverseIterator(List *list);
+
+void *previousElement(ListIterator *iter);
+
+#endif
